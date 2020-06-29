1d31c29b0aebc8c96885ab1aa25dd2fd8ebc9820
Rihards Novickis <rihards.novickis@edi.lv>
2020-06-29 14:21:07 +0300

    Updated simple communications test

--- tests/simpleCommunication/src/main.c
+++ tests/simpleCommunication/src/main.c
@@ -6,14 +6,15 @@
 #include "timer.h"
 
 #define _I(fmt, args...)   printf(fmt "\n", ##args); fflush(stdout)
-#define PAYLOAD_SIZE 1024*1024
+#define PAYLOAD_SIZE    1024*1024
+#define TRANSFER_COUNT  8*1024
 
 
 void *thread_push(void *arg){
     icom_t *icom = icom_initPush((char*)arg, PAYLOAD_SIZE, 2, 0);
 
     icomPacket_t *packet = icom_getCurrentPacket(icom);
-    for(int i=0; i<1024; i++){
+    for(int i=0; i<TRANSFER_COUNT; i++){
         //printf("Sending(%s) %d\n", (char*)arg, i);
         ((int*)(packet->payload))[0] = i;
         packet = icom_do(icom);
@@ -27,7 +28,7 @@ void *thread_pull(void *arg){
     icom_t *icom = icom_initPull((char*)arg, PAYLOAD_SIZE, 0);
 
     icomPacket_t *packet = icom_getCurrentPacket(icom);
-    for(int i=0; i<1024; i++){
+    for(int i=0; i<TRANSFER_COUNT; i++){
         packet = icom_do(icom);
         do{
             //printf("Received(%s): %u\n", (char*)arg, ((int*)packet->payload)[0]);
@@ -44,7 +45,7 @@ void *thread_pushZero(void *arg){
     icom_t *icom = icom_initPush((char*)arg, PAYLOAD_SIZE, 2, ICOM_ZERO_COPY);
 
     icomPacket_t *packet = icom_getCurrentPacket(icom);
-    for(int i=0; i<1024; i++){
+    for(int i=0; i<TRANSFER_COUNT; i++){
         //printf("Sending(%s) %d\n", (char*)arg, i);
         ((int*)(packet->payload))[0] = i;
         packet = icom_do(icom);
@@ -58,7 +59,7 @@ void *thread_pullZero(void *arg){
     icom_t *icom = icom_initPull((char*)arg, PAYLOAD_SIZE, ICOM_ZERO_COPY);
 
     icomPacket_t *packet = icom_getCurrentPacket(icom);
-    for(int i=0; i<1024; i++){
+    for(int i=0; i<TRANSFER_COUNT; i++){
         packet = icom_do(icom);
         do{
             //printf("Received(%s): %u\n", (char*)arg, ((int*)packet->payload)[0]);
@@ -71,11 +72,41 @@ void *thread_pullZero(void *arg){
 }
 
 
+void *thread_pushZeroProtected(void *arg){
+    icom_t *icom = icom_initPush((char*)arg, PAYLOAD_SIZE, 2, ICOM_ZERO_COPY | ICOM_PROTECTED);
+
+    icomPacket_t *packet = icom_getCurrentPacket(icom);
+    for(int i=0; i<TRANSFER_COUNT; i++){
+        //printf("Sending(%s) %d\n", (char*)arg, i);
+        ((int*)(packet->payload))[0] = i;
+        packet = icom_do(icom);
+    }
+
+    icom_deinit(icom);
+    return NULL;
+}
+
+void *thread_pullZeroProtected(void *arg){
+    icom_t *icom = icom_initPull((char*)arg, PAYLOAD_SIZE, ICOM_ZERO_COPY | ICOM_PROTECTED);
+
+    icomPacket_t *packet = icom_getCurrentPacket(icom);
+    for(int i=0; i<TRANSFER_COUNT; i++){
+        packet = icom_do(icom);
+        do{
+            //printf("Received(%s): %u\n", (char*)arg, ((int*)packet->payload)[0]);
+            packet = packet->next;
+        } while(packet != NULL);
+    }
+
+    icom_deinit(icom);
+    return NULL;
+}
+
 
 int main(void){
     pthread_t pidTx, pidRx;
     icom_t *icomPush, *icomPull;
-    unsigned timeDeep, timeZero;
+    unsigned timeDeep, timeZero, timeZeroProtected;
 
     _I("Initializing icom API");
     icom_init();
@@ -102,8 +133,20 @@ int main(void){
     timeZero = timer_us_stop();
 
 
-    _I("TIME (with deep copy): %u us", timeDeep);
-    _I("TIME (with zero copy): %u us", timeZero);
+    _I("Initializing pthreads (zero data protected copy experiment)");
+    timer_us_start();
+    pthread_create(&pidTx, NULL, thread_pushZeroProtected, "inproc://tmp");
+    pthread_create(&pidRx, NULL, thread_pullZeroProtected, "inproc://tmp");
+
+    _I("Waiting for threads to finish");
+    pthread_join(pidTx, NULL);
+    pthread_join(pidRx, NULL);
+    timeZeroProtected = timer_us_stop();
+
+
+    _I("TIME (with deep copy):           %u us", timeDeep);
+    _I("TIME (with zero copy):           %u us", timeZero);
+    _I("TIME (with zero copy, prtected): %u us", timeZeroProtected);
 
     _I("Deinitializing icom API");
     icom_release();
