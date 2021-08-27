cc2faab8ccd8653363f05d3e0494e647653e4e6b
Rihards Novickis <rihards.novickis@edi.lv>
2021-08-27 16:15:38 +0300

    Passed arguments to the testing framework

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "gtest/gtest.h"
 
-int main(void){
-  testing::InitGoogleTest();
+int main(int argc, char *argv[]){
+  testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
