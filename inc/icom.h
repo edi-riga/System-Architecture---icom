53c5e486332c457990391bf866b9cbd23b327096
Rihards Novickis <rihards.novickis@edi.lv>
2021-09-16 01:51:34 +0300

    Added initial documentation for part of ICOM API

--- inc/icom.h
+++ inc/icom.h
@@ -9,40 +9,65 @@
 
 /* forward declarations */
 typedef struct icomLink icomLink_t;
+typedef struct icom icom_t;
 
 /* icom flags data type */
 typedef uint32_t icomFlags_t;
 
-/* icom communication header */
+
+/** @brief The main icom (internal communication) encapsulation object */
+typedef struct icom {
+  icomType_t    type;            /** communication type */
+  icomFlags_t   flags;           /** communication flags */
+  unsigned      comCount;        /** number of communication links */
+  icomLink_t  **comConnections;  /** communication links */
+  char        **comStrings;      /** strings for the communication links */
+} icom_t;
+
+/** @brief The header of any communication link which is sent before any
+ *  actual data transfer */
 typedef struct {
-  icomType_t   type;    // 4 bytes
-  icomFlags_t  flags;   // 4 bytes
-  uint32_t     bufSize; // 4 bytes
+  icomType_t   type;    /** communication type (4 bytes) */
+  icomFlags_t  flags;   /** communication flags (4 bytes) */
+  uint32_t     bufSize; /** upcomming buffer size (4 bytes) */
 } icomMsgHeader_t;
 
-
+/** @brief Generic encapsulation object for any communication link */
 typedef struct icomLink {
-  const char  *comString;
-  void        *pdata;
-  icomType_t   type;
-  icomFlags_t  flags;
-  void        *recvBuf;
-  uint32_t     recvBufSize;
+  const char  *comString; /** communication string */
+  void        *pdata;     /** private communication link's data */
+  icomType_t   type;      /** communication type */
+  icomFlags_t  flags;     /** communication flags */
+  void        *recvBuf;     /** received data buffer */
+  uint32_t     recvBufSize; /** received data buffer size */
   icomStatus_t (*sendHandler)(icomLink_t *link, void *buf, unsigned bufSize);
   icomStatus_t (*recvHandler)(icomLink_t *link, void **buf, unsigned *bufSize);
 } icomLink_t;
 
-typedef struct {
-  icomType_t    type;
-  icomFlags_t   flags;
-  unsigned      comCount;
-  icomLink_t  **comConnections;
-  char        **comStrings;
-} icom_t;
 
-
-
+/** @brief Initializes icom communication object. The communication object
+ *         supports multiple and different links, e.g. socket, fifo, etc.
+ *         The message can be sent to all links by using the icom_send routine.
+ *
+ *  @param comString String used for the actual creation of the communication
+ *         links. Description of a single link is expressed as "type:comId",
+ *         for example, Linux connect socket's string would be
+ *         "socket_tx:127.0.0.1:99999". Multiple connections can be made either
+ *         by comma-separating individual communications or by using special
+ *         range syntax: "socket_tx:127.0.0.1:[9988-9999]"
+ *  @param flags Determines the underlying configuration of the links
+ *         implementation.
+ *
+ *  @return On success returns icom object. Otherwise on error, the
+ *        ICOM_IS_ERR(ptr) returns true, and the ICOM_PTR_ERR(ptr)
+ *        returns the actual icomStatus_t error code.
+ */
 icom_t* icom_init(const char *comString, icomFlags_t flags);
+
+/** @brief Deinitializes icom communication object.
+ *
+ *  @param icom Pointer to the icom communication object.
+ */
 void icom_deinit(icom_t* icom);
 
 icomStatus_t icom_do(icom_t *icom);
@@ -53,7 +78,6 @@ icomStatus_t icom_setBuffer2(icom_t *icom, void *buf);
 icomStatus_t icom_setBuffer3(icom_t *icom, void *buf, unsigned bufSize);
 icomStatus_t icom_getBuffer2(icom_t *icom, void **buf);
 icomStatus_t icom_getBuffer3(icom_t *icom, void **buf, unsigned *bufSize);
-
 
 #define icom_setBuffer(...) \
   CONCATENATE(icom_setBuffer,ARGUMENT_COUNT(__VA_ARGS__)(__VA_ARGS__)
