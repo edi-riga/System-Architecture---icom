2e50e3bce6272263b400c5338f2ee70c06a595fa
Rihards Novickis <rihards.novickis@edi.lv>
2021-09-16 01:17:02 +0300

    Added tests for varying message size

--- tests/src/link_socket.cpp
+++ tests/src/link_socket.cpp
@@ -65,3 +65,61 @@ TEST(link_socket, send_recv_simple){
   icom_deinit(icomConnect);
   icom_deinit(icomBind);
 }
+
+
+TEST(link_socket, send_recv_varying){
+  icom_t *icomBind, *icomConnect;
+  icomStatus_t status;
+  thread_send_t thread_pdata;
+  pthread_t pid;
+  char *rxBuf, txBuf[4] = "abc";
+  uint32_t rxBufSize, txBufSize = sizeof(txBuf);
+  void *pthreadRet;
+
+  icomConnect = icom_init("socket_tx:127.0.0.1:8890", 0);
+  EXPECT_FALSE(ICOM_IS_ERR(icomConnect));
+
+  icomBind = icom_init("socket_rx:*:8890", 0);
+  EXPECT_FALSE(ICOM_IS_ERR(icomBind));
+
+
+  thread_pdata = {icomConnect, txBuf, txBufSize};
+  pthread_create(&pid, NULL, thread_send, &thread_pdata);
+
+  status = icom_recv(icomBind, (void**)&rxBuf, &rxBufSize);
+  EXPECT_TRUE(status == ICOM_SUCCESS);
+  EXPECT_TRUE(rxBufSize == txBufSize);
+  EXPECT_STREQ(rxBuf, txBuf);
+
+  pthread_join(pid, &pthreadRet);
+  EXPECT_TRUE((icomStatus_t)(unsigned long long)pthreadRet == ICOM_SUCCESS);
+
+
+  txBuf[2] = '\0';
+  thread_pdata = {icomConnect, txBuf, 2};
+  pthread_create(&pid, NULL, thread_send, &thread_pdata);
+
+  status = icom_recv(icomBind, (void**)&rxBuf, &rxBufSize);
+  EXPECT_TRUE(status == ICOM_SUCCESS);
+  EXPECT_TRUE(rxBufSize == 2);
+  EXPECT_STREQ(rxBuf, txBuf);
+
+  pthread_join(pid, &pthreadRet);
+  EXPECT_TRUE((icomStatus_t)(unsigned long long)pthreadRet == ICOM_SUCCESS);
+
+
+  txBuf[1] = '\0';
+  thread_pdata = {icomConnect, txBuf, 1};
+  pthread_create(&pid, NULL, thread_send, &thread_pdata);
+
+  status = icom_recv(icomBind, (void**)&rxBuf, &rxBufSize);
+  EXPECT_TRUE(status == ICOM_SUCCESS);
+  EXPECT_TRUE(rxBufSize == 1);
+  EXPECT_STREQ(rxBuf, txBuf);
+
+  pthread_join(pid, &pthreadRet);
+  EXPECT_TRUE((icomStatus_t)(unsigned long long)pthreadRet == ICOM_SUCCESS);
+
+  icom_deinit(icomConnect);
+  icom_deinit(icomBind);
+}
