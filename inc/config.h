c196b9d4fc943f9de93927eee01e795116356feb
Rihards Novickis <rihards.novickis@edi.lv>
2021-12-27 19:51:41 +0200

    Added SEPERATOR define

--- inc/config.h
+++ inc/config.h
@@ -3,6 +3,17 @@
 
 /* The maximum string length, which can be read using scan for the communication
  * type identifier. */
-#define MAX_TYPE_STRING_LENGTH 63
+#ifndef MAX_TYPE_STRING_LENGTH
+  #define MAX_TYPE_STRING_LENGTH 63
+#endif
+
+/* Delimeter character for settting ICOM communication strings, the default
+ * format is: "<type>|<flags>|<communication-string>"
+ * Examples:
+ *   - "socket_tx|default|127.0.0.1:8888"
+ *   - "socket_rx|default|*:8888"*/
+#ifndef ICOM_DELIMITER
+  #define ICOM_DELIMITER  '|'
+#endif
 
 #endif
