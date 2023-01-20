317062d9b5127523f24a032f182705fd98bda375
rihards.novickis <rihards.novickis@edi.lv>
2023-01-20 19:01:09 +0200

    ../tests/src/link_common.cpp

--- tests/inc/link_common.h
+++ tests/inc/link_common.h
@@ -4,9 +4,8 @@
 void link_common_initialization(const char *icomStr, unsigned testCount);
 void link_common_simple(const char *icomTxStr, const char *icomRxStr, uint32_t txBufSize);
 void link_common_varied(const char *icomTxStr, const char *icomRxStr, unsigned testCount);
-void link_common_fanin(const char *connectStrings[], const char *bindString, unsigned connectCount);
-void link_common_complex(const char *connectStrings[], const char *bindStrings[]);
 void link_common_timeout_rx(const char *icomRxStr, const char *icomTxStr);
 void link_common_timeout_tx(const char *icomRxStr, const char *icomTxStr);
+void link_common_topology(std::vector<const char*> icomRxStr, std::vector<const char*> icomTxStr, uint32_t transfers);
 
 #endif
